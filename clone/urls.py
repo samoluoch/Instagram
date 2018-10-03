97a9ad62dcb8c7272ce61174613003cc6c83ad69
Sam Oluoch <samsoluoch@gmail.com>
2018-10-03 11:13:51 +0300

    created instagram.html template that will display all images, created views and named instagram for initiating the first view on the instagram.html template

---
+++ clone/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^instagram/',views.instagram,name = 'instagram'),
+    # url(r'^$',views.all_images,name = 'image'),
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImages'),
+    # url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^image/(\d+)',views.image,name = 'specificImage'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
