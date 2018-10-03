e22cc4748bfb63b433287ea5582b1f6933ddd70a
Sam Oluoch <samsoluoch@gmail.com>
2018-10-03 10:40:24 +0300

    first commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/MacbookAir/Desktop/MORINGA/CORE/PYTHON/DJANGO/Instagram/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
