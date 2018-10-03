b7f4a9f1b1c40ffa4836f7bff973d68074e11c60
Sam Oluoch <samsoluoch@gmail.com>
2018-10-03 11:58:41 +0300

    created a function that will query all images and render them to the instagram.html template

--- clone/views.py
+++ clone/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def instagram(request):
     return render(request, 'instagram.html')
+
+
+def all_images(request):
+    images = Image.objects.all()
+
+    return render(request, "all-images/today-images.html", {"images": images})
