dfc0cf132c7efa087b7badb8e54c4ad36ad8085b
Sam Oluoch <samsoluoch@gmail.com>
2018-10-03 17:22:48 +0300

    extended base.html to instagram.html. Displayed the navbar to the instagram.html. Created views that displays all images to the instagram.html template

--- clone/views.py
+++ clone/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import Image
+
+import datetime as dt
 
 # Create your views here.
 def instagram(request):
@@ -9,3 +13,8 @@ def all_images(request):
     images = Image.objects.all()
 
     return render(request, "all-images/today-images.html", {"images": images})
+
+# def image_of_day(request):
+#     date = dt.date.today()
+#     images = Image.todays_image()
+#     return render(request, 'instagram.html', {"date": date, "images": images})
