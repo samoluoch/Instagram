ae9d5b56c89040a4935236ab636b25fa781970ad
Sam Oluoch <samsoluoch@gmail.com>
2018-10-04 08:05:30 +0300

    created a view function that fetches profiles from the db and returns them

--- clone/views.py
+++ clone/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
-from .models import Image
+from .models import Image,Profile
+from django.contrib.auth.models import User
 
 import datetime as dt
 
@@ -18,3 +19,15 @@ def all_images(request):
 #     date = dt.date.today()
 #     images = Image.todays_image()
 #     return render(request, 'instagram.html', {"date": date, "images": images})
+
+
+def profile(request, username):
+    profile = User.objects.get(username=username)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    images = Image.get_profile_images(profile.id)
+    title = f'@{profile.username} Instagram photos and videos'
+
+    return render(request, 'profile/profile.html', {'title':title, 'profile':profile, 'profile_details':profile_details, 'images':images})
