cac70c4aba81121fc417ac40f061ed301e50bdc0
Sam Oluoch <samsoluoch@gmail.com>
2018-10-05 20:13:32 +0300

    created comments form that allows users to create comments on images

--- clone/forms.py
+++ clone/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile,Comments
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -21,3 +21,8 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    class CommentForm(forms.ModelForm):
+        class Meta:
+            model = Comments
+            exclude = ['image', 'user']
