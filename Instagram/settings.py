2a4f7744f40ab2fe3752f6ef2e39df680850ad6d
Sam Oluoch <samsoluoch@gmail.com>
2018-10-08 10:16:05 +0300

    attempted deployment

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clone.apps.CloneConfig',
+    # 'clone.apps.CloneConfig',
+    'clone',
     'bootstrap3',
     'tinymce',
 
